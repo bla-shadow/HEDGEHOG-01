637c6bfc74bb689599b13053bdb7c501427eb7c6
sami-geni <mollyjjk8@gmail.com>
2026-04-16 09:40:55 +0100

    Update autorespondv.js

--- scripts/cmds/autorespondv.js
+++ scripts/cmds/autorespondv.js
@@ -1,4 +1,4 @@
- module.exports = {
+module.exports = {
   config: {
     name: "autorespondv3",
     version: "1.4",
@@ -19,24 +19,28 @@
     const { body, messageID, threadID } = event;
     if (!body) return;
 
-    const emojis = {
+    const emojis = { 
+   
+      "💀":["kick", "bloque", "block", "banni", "ban", "out"],
+      "😘":["bisous", "bisou", "kiss"],
+      "🥰":["mignonne", " mignon", "belle", "beau"],
       "💜": ["cliff", "august", "jonell", "david", "purple", "fritz", "sab", "haru", "xuazane", "kim"],
       "💚": ["dia", "seyj", "ginanun", "gaganunin", "pfft", "xyrene", "gumanun"],
       "😾": ["jo", "ariii", "talong", "galit"],
       "😼": ["wtf", "fck", "haaays", "naku", "ngi ", "ngek", "nge ", "luh", "lah"],
       "😸": ["pill", "laugh", "lt ", "gagi", "huy", "hoy"],
       "🌀": ["prodia", "sdxl", "bardv3", "tanongv2", "-imagine", "genimg", "tanongv4", "kamla", "-shortcut", "imagine", "textpro", "photofy"],
-      "👋": ["hi ", "hello", "salut", "bjr", "bonjour", "bonsoir", "slt"],
-      "🔥": ["astig", "damn", "angas", "galing", "husay", ".jpg"],
+      "👋": ["hi ", "hello", "salut", "bjr", "bonjour", "bonsoir", "slt","bsr"],
+      "🔥": ["astig", "damn", "angas", "galing", "husay", ".jpg","feu","chaud","flamme"],
       "💩": ["merde", "caca", "shit"],
-      "🤢": ["beurk", "dégueulasse", "dégeu", "horrible", "vomir"],
+      "🤢": ["beurk", "dégueulasse", "dégeu", "horrible", "vomir"," monstrueuse"],
       "🌸": ["amour", "câlin", "tendresse", "gentillesse", "bienveillance", "douceur", "complicité", "gratitude", "bonheur", "amitié"],
       "😂": ["ridicule", "clownesque", "farce", "pitrerie", "comique", "drôle", "amusant", "hilarant", "loufoque", "bouffonnerie", "cocasse", "burlesque", "rigolo", "absurde", "irrévérencieux", "ironique", "parodie", "esprit", "facétieux"],
-      "😎": ["cool", "formidable", "😎"],
-      "⚡": ["super", "aesther"],
-      "🤖": ["prefix", "robot"],
+      "😎": ["cool", "formidable", "😎","classe","superbe","chic","splendide"],
+      "⚡": ["super", "aesther"," éclair","tonnerre"],
+      "🤖": ["prefix", "robot","shadow","bot",],
       "🔰": ["nathan", "cyble", "barro", "personnage"],
-      "✔️": ["bien", "ok"],
+      "✅": ["bien", "ok","okay","d'accord","carré"],
       "🎉": ["congrats", "félicitation", "goddess-anaïs"],
       "📑": ["disertation", "liste", "document", "playlist", "count all"],
       "♻️": ["restart", "revoila"],
@@ -53,9 +57,9 @@
       "💙": ["manga", "anime", "sukuna"],
       "😕": ["bruh"],
       "👎": ["kindly provide"],
-      "🌩️": ["*thea", "tatakae", "damare"],
-      "😈": ["malin", "devil", "evil", "suprem", "sadique"],
-      "🔪": ["tué"]
+      "🌩️": ["*thea", "foudre", "damare"],
+      "😈":["malin", "devil", "evil", "suprem", "sadique"],
+      "🔪": ["tué","meurtre","meurt", "mourir","dead","die"],   
     };
 
     const replies = {
